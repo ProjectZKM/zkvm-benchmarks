3fec9ecae1a4c34fadab2f26fd9a768a5a1b6438
eigmax <stephen@ieigen.com>
2024-09-18 23:56:49 +0800

    fix: breakchanges

--- risczero/sha2-chain/host/src/main.rs
+++ risczero/sha2-chain/host/src/main.rs
@@ -22,7 +22,7 @@ fn bench_sha2_chain(iters: u32) -> (Duration, usize) {
     let prover = LocalProver::new("prover");
 
     let start = std::time::Instant::now();
-    let receipt = prover.prove(env, SHA2_CHAIN_BENCH_ELF).unwrap();
+    let receipt = prover.prove(env, SHA2_CHAIN_BENCH_ELF).unwrap().receipt;
     let end = std::time::Instant::now();
     let duration = end.duration_since(start);
 
