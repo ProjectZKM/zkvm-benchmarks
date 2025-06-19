8e7660bcc2f13ef704085a4a8e3dc54c8fe4c62a
Blake <blake.h@zkm.io>
2025-06-20 00:53:15 +0800

    add mul2048

---
+++ zkm2/src/bin/mul2048.rs
@@ -0,0 +1,14 @@
+use utils::benchmark_v2;
+use zkm_script::{benchmark_mul2048, init_logger};
+
+fn main() {
+    init_logger();
+    let iters = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+
+    benchmark_v2(
+        benchmark_mul2048,
+        &iters,
+        "../benchmark_outputs/mul2048_zkm2.csv",
+        "iters",
+    );
+}
